b2afb6df63bc9c1bab392e74ed4dfcd913cc1e95
moriya1232 <moriya1232@gmail.com>
2021-01-10 14:24:58 +0200

    seif 1

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import pandas as pd
+m=0
+C = 1
 
 def get_simply_recommendation(k):
-    metadata = pd.read_csv("books.csv", encoding='latin-1', low_memory=False)
-    metadata.head(k)
+    global m
+    global C
+    book_ids = pd.read_csv("books.csv", encoding='ISO-8859-1', usecols=['book_id', 'title', 'books_count'])
+    ratings = pd.read_csv("ratings.csv", encoding='ISO-8859-1')
+    users = pd.read_csv("users.csv", encoding='ISO-8859-1')
 
-
-    book_ids = pd.read_csv("books.csv", encoding='latin-1', usecols=['book_id'])
-    ratings = pd.read_csv("ratings.csv", encoding='latin-1')
-    users = pd.read_csv("users.csv", encoding='latin-1')
-
-    # Ratings_mean = ratings.groupby(['book_id'])[['rating']].mean().rename(
-    #     columns={'rating': 'Mean_rating'}).reset_index()
-
-    ratings_sum = ratings.groupby(['book_id'])[['rating']].sum().rename(columns={'book_id': 'sum_rating'}).reset_index()
-    ratings_num = ratings.groupby(['book_id'])[['rating']]
-    #print(ratings_num)
-    for book in ratings_num.groups:
-        print(book)
-
+    average_ratings = ratings.groupby(['book_id'])[['rating']].mean().rename(columns={'rating': 'average_rating'}).reset_index()
+    C = average_ratings['average_rating'].mean()
+    num_votes = pd.DataFrame(data=ratings.groupby(['book_id']).count()['user_id']).rename(columns={'user_id': 'num_votes'})
+    metadata = pd.DataFrame(zip(book_ids.book_id, book_ids.title, num_votes.num_votes, average_ratings.average_rating)).rename(columns={0: 'book_id', 1: 'title', 2: 'vote_count', 3: 'vote_average'})
+    m = metadata['vote_count'].quantile(0.90)
+    q_books = metadata.copy().loc[metadata['vote_count'] >= m]
+    q_books.insert(4, "score", None)
+    q_books['score'] = q_books.apply(weighted_rating, axis=1)
+    q_books = q_books.sort_values('score', ascending=False)
+    result = q_books[['title', 'vote_count', 'vote_average', 'score']].head(k)
+    print(result)
 
 def average(lst):
     return sum(lst) / len(lst)
 
 # Function that computes the weighted rating of each movie
-def weighted_rating(x, m, C):
+def weighted_rating(x, m=m, C=C):
     v = x['vote_count']
     R = x['vote_average']
     # Calculation based on the IMDB formula
