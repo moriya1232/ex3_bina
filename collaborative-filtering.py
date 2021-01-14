f94cca24e6f627b9b98a684cca6fa9498bbeeeb0
moriya1232 <moriya1232@gmail.com>
2021-01-14 03:26:50 +0200

    part b

--- collaborative-filtering.py
+++ collaborative-filtering.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 import heapq
-
-import sklearn
+from scipy.spatial import distance
 from sklearn.metrics.pairwise import pairwise_distances
 
 def get_top_rated(data_matrix_row, items, k=10):
-  srt_idx = np.argsort(-data_matrix_row)
-  #print(~np.isnan(data_matrix_row[srt_idx]))
-  srt_idx_not_nan = srt_idx[~np.isnan(data_matrix_row[srt_idx])]
-  return items['title'].iloc[srt_idx_not_nan][:k]
+    srt_idx = np.argsort(-data_matrix_row)
+    srt_idx_not_nan = srt_idx[~np.isnan(data_matrix_row[srt_idx])]
+    ids = items['book_id'].iloc[srt_idx_not_nan][:k]
+    titles=items['title'].iloc[srt_idx_not_nan][:k]
+    return pd.concat([ids, titles], axis=1)
 
 def keep_top_k(arr, k):
     smallest = heapq.nlargest(k, arr)[-1]
@@ -19,17 +17,10 @@ def keep_top_k(arr, k):
     return arr
 
 def build_CF_prediction_matrix(sim):
-    # cosine = sklearn.metrics.pairwise.cosine_similarity(x,y)
-    # euclidean = np.linalg.norm(x-y)
-    # jaccard = sklearn.metrics.jaccard_score(x,y)
     users = pd.read_csv("users.csv", encoding='ISO-8859-1').sort_values(by="user_id")
     ratings = pd.read_csv("ratings.csv", encoding='ISO-8859-1')
     books = pd.read_csv("books.csv", encoding='ISO-8859-1', usecols=['book_id', 'title'])
-    ## sort_by_user_id = ratings.sort_values(by="user_id")
-    ## columns_id_books = books.pivot_table(columns='book_id', aggfunc='first')
-    ## columns_id_books.insert(0, "user_id", None)
-
-    # calculate the number of unique users and movies.
+    # calculate the number of unique users and books.
     n_users = ratings.user_id.unique().shape[0]
     n_items = books.book_id.unique().shape[0]
 
@@ -52,29 +43,48 @@ def build_CF_prediction_matrix(sim):
     ratings_diff[np.isnan(ratings_diff)] = 0
 
     # calculate user x user similarity matrix
-    user_similarity = 1 - pairwise_distances(ratings_diff, metric=sim)
-    return user_similarity
 
+    if sim == "jaccard":
+        user_similarity = 1 - pairwise_distances(ratings_diff, metric=distance.jaccard)
+    else:
+        try:
+            user_similarity = 1 - pairwise_distances(ratings_diff, metric=sim)
+        except:
+            print("sim illegal!")
+            return
 
+    # For each user (i.e., for each row) keep only k most similar users, set the rest to 0.
+    # Note that the user has the highest similarity to themselves.
+    k=10
+    user_similarity = np.array([keep_top_k(np.array(arr), k) for arr in user_similarity])
+    # since n-k users have similarity=0, for each user only k most similar users contribute to the predicted ratings
+    pred = mean_user_rating + user_similarity.dot(ratings_diff) / np.array([np.abs(user_similarity).sum(axis=1)]).T
+    pred.round(2)
+    return pred, data_matrix, list_of_users
 
 def get_CF_recommendation(user_id, k):
     books = pd.read_csv("books.csv", encoding='ISO-8859-1', usecols=['book_id', 'title'])
-    user = user_id - 1
     cosine = "cosine"
     euclidean = "euclidean"
-    # TODO: check the problem with jaccard
     jaccard = "jaccard"
-    data_matrix = build_CF_prediction_matrix(euclidean)
-    data_matrix_row = data_matrix[user]
+    user_similarity, data_matrix, list_of_users = build_CF_prediction_matrix(jaccard)
+    user = list_of_users.index(user_id)
 
-    result = get_top_rated(data_matrix_row, books, k)
+    prediction_row = user_similarity[user]
+    original_ratings_row = data_matrix[user]
+
+    # remove raitings for the book that the user read
+    prediction_row[~np.isnan(original_ratings_row)] = 0
+
+    result = get_top_rated(prediction_row, books, k)
+    result = result.set_index('book_id')
     print(result)
     return result
-
 
 def main():
     # build_CF_prediction_matrix("cosine")
     get_CF_recommendation(511, 10)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
